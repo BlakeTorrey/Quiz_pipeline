15a2a40f1449f837d1c4ec655fae258dd82bd222
Blake Torrey <Btorrey1998@gmail.com>
2025-02-05 19:05:59 -0700

    fixed some errors that were preventing deployment.

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,23 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        const model = models[modelName];
+        if (!model) {
+            throw new Error(`Model ${modelName} does not exist.`);
+        }
+        if (!model?.db?.db) {
+            throw new Error('Database connection not found');
+        }
+        let modelExists = await model.db.db.listCollections({
+            name: collectionName
+        }).toArray();
+        if (modelExists.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        console.error("Error in cleanDb function:", err);
+        throw err;
+    }
+};
--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,25 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    const model = models[modelName];
+
+    if (!model) {
+      throw new Error(`Model ${modelName} does not exist.`);
+    }
+
+    if (!model?.db?.db) {
+      throw new Error('Database connection not found');
+    }
+
+    let modelExists = await model.db.db.listCollections({
       name: collectionName
-    }).toArray()
+    }).toArray();
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
+    console.error("Error in cleanDb function:", err);
     throw err;
   }
-}
+};
